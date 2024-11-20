53c40bbc601e52ebcd8471e82b829aaa0aab0da3
Vraj Nena <vnen0001@student.monash.edu>
2024-11-20 12:34:25 +1100

    ADD: Variables for terraform

---
+++ infrastructure/variables.tf
@@ -0,0 +1,41 @@
+variable "project_id" {
+    description = "The selected project id for GCP"
+    type =  string
+}
+
+variable "region" {
+    description = "The region of GCP"
+    type = string
+    default = "us-central1"
+}
+variable "zone" {
+    description = "The zone in which we want resources"
+    type = string
+
+}
+variable "machine_type" {
+    description = "The type of VM needed"
+    type = string
+}
+variable "image_vm" {
+    description = "The OS image for our VM"
+    type = string
+    default = "ubuntu-os-cloud/ubuntu-2204-lts"
+}
+
+variable "disk_size" {
+    description = "The size of disk"  
+    type = number
+}
+variable "gcp_service_account" {
+    type = string
+    description = "The service account to use"
+}
+
+variable "ssh_key" {
+    description = "The public key for VM"
+    type = string
+}
+variable "ssh_user" {
+    type = string  
+}
